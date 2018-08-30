db2263fbd00bf36b3fef3475edd72c8d1dde6de2
d4n13lbc <daniel.barragan@correo.icesi.edu.co>
2018-08-30 01:25:54 -0500

    Update tests

--- 02_array_response/gm_analytics/handlers.py
+++ 02_array_response/gm_analytics/handlers.py
@@ -1,6 +1,11 @@
-def get_user_info(username=None):
+import logging
+
+def get_user_info(org, repo, username):
+    logging.debug('org=%s repo=%s username=%s',org, repo, username)
     return {"user_info": username}
 
-def get_commits_info(username=None, time_range=None):
+def get_commits_info(org, repo, username, time_range=30):
+    logging.debug('org=%s repo=%s username=%s',org, repo, username)
+    logging.debug('time_range=%s', time_range)
     return [{'commits_count': 10, 'yyyymmdd_date': '20180101'},
             {'commits_count': 10, 'yyyymmdd_date': '20180102'}]
--- 03_unit_tests/tests/test_handlers.py
+++ 03_unit_tests/tests/test_handlers.py
@@ -1,40 +1,34 @@
 import pytest
-import connexion
 import mock
-import pdb
+import connexion
 from gm_analytics import handlers
-
-flask_app = connexion.FlaskApp(__name__)
-# flask_app.add_api('swagger.yml')
-flask_app.add_api('/home/pi/so-microservices-python-part2/03_unit_tests/gm_analytics/swagger/indexer.yaml')
 
 
 @pytest.fixture(scope='module')
 def client():
+    flask_app = connexion.FlaskApp(__name__)
     with flask_app.app.test_client() as c:
         yield c
-
-# @pytest.fixture
-# def client():
-    # flask_app = connexion.FlaskApp(__name__)
-    # flask_app.add_api('/home/pi/so-microservices-python-part2/03_unit_tests/gm_analytics/swagger/indexer.yaml')
-#     return flask_app.app.test_client()
 
 
 @pytest.fixture
 def user_info():
-    return {'user_info': 'dummy.user'}
+    return {'user_info': 'some_info'}
 
 
-def test_get_user_info(mocker, client, user_info):
+def test_get_health(client):
+    # GIVEN ...
+    # WHEN I access to the url ...
+    # THEN ...
+    response = client.get('/health')
+    assert response.status_code == 200
+
+
+def test_get_user_info(mocker, user_info):
     # GIVEN not query parameters or payload
     # WHEN I access to the url GET /users/daniel.barragan
     # THEN the information for an user must be returned
     mocker.patch.object(handlers, 'get_user_info', return_value=user_info)
-    response = client.get('/users/daniel.barragan')
-    pdb.set_trace()
-    assert {"user_info": "dummy.user"} in response.data
-    assert response.status_code == 200
+    user_info = handlers.get_user_info('d4n13lbc')
+    assert {'user_info': 'some_info'} == user_info
 
-#  https://stackoverflow.com/questions/42934525/how-to-test-a-connexion-flask-app
-# https://github.com/pallets/flask/tree/1.0.2/examples/tutorial/tests
